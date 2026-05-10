2c7952dbef680395d1a675c0afbbeca0c28040a9
githua976sys <githua976@gmail.com>
2026-05-10 17:20:16 +0300

    added greeting component to check collaboration

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Greeting from "./Component/Greeting";
 import Header from "./Component/Header";
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <>
     <h1 className="text-blue-700 text-center text-4xl">Hello React!</h1>;
     <Header/>
+    <Greeting/>
     </>
   )
 }
---
+++ src/Component/Greeting.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Greeting() {
+  return (
+<>
+<div>Hello, World!</div>
+</>  )
+}
+
+export default Greeting
