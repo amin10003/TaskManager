38b1c2d57d8a008d70ab0c1cd1a2c246ad1291b1
nimrod <nimrodtitus12@gmail.com>
2026-05-10 17:12:53 +0300

    created header component to check collaboration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./Component/Header";
+
 function App() {
-  return <h1 className="text-blue-700 text-center text-4xl">Hello React!</h1>;
+  return (
+    <>
+    <h1 className="text-blue-700 text-center text-4xl">Hello React!</h1>;
+    <Header/>
+    </>
+  )
 }
 
 export default App;
