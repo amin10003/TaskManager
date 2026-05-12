d7b1535f996c45f0f4dcc08a3e91fecf7c7a45ae
githua976sys <githua976@gmail.com>
2026-05-12 17:56:21 +0300

    Created all files needed in the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { Route , Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import NavBar from './components/ui/Navbar'
+import ProductDetails from './Pages/ProductDetails'
+import Cart from './Pages/Cart'
+import Checkout from './Pages/Checkout'
 
 function App() {
   return (
    <>
-   <Home/>
+  <NavBar/>
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path="/product/:id" element={<ProductDetails/>} />
+    <Route path="/cart" element={<Cart/>} />
+    <Route path="/checkout" element={<Checkout/>} />
+  </Routes>
    </>
   )
 }
