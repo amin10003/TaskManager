78fee7deacec2f53885af4c5f7bf4a04be68f55c
githua976sys <githua976@gmail.com>
2026-05-12 16:04:02 +0300

    add Pages

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 function Navbar() {
   return (
     <>
+    <nav className="bg-black text-white p-4 flex gap-6">
+       < Link to ="/">Home</Link>
+       < Link to ="/cart">Cart</Link>
+      <Link to ="/login">Login</Link>
+
+    </nav>
     </>
   )
 }
