88d74ca773c3f494060b58d7f5aac6a284b2da0e
amin10003 <maxamedxasancali10003@gmail.com>
2026-05-13 17:34:37 +0300

    implement remove from cart functionality

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { CartContext } from "@/Context/CartContext";
 
 function Cart() {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, dispatch } = useContext(CartContext);
 
   return (
     <div className="p-6">
@@ -24,6 +24,17 @@ function Cart() {
               <h2 className="font-semibold">{item.title}</h2>
 
               <p className="font-bold">${item.price}</p>
+              <button
+                onClick={() =>
+                  dispatch({
+                    type: "REMOVE_FROM_CART",
+                    payload: item.id,
+                  })
+                }
+                className="border px-4 py-2 rounded-lg"
+              >
+                Remove
+              </button>
             </div>
           </div>
         ))}
