6cc7b0d91d67eafd27e465e0c3810d4987fa7954
amin10003 <maxamedxasancali10003@gmail.com>
2026-05-13 06:57:33 +0300

    fix router provider and navbar navigation links

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-black text-white p-4 flex justify-between items-center">
-<h1 className="text-xl font-bold">
-    E-commerce
-</h1>
-<div className="flex gap-4 text-2xl text-white">
-<Link to ="/" Home>Home</Link>
-<Link to ="/cart" Cart>Cart</Link>
-<Link to ="/login" Login>login</Link>
-</div>
+      <h1 className="text-xl font-bold">
+        E-commerce
+      </h1>
+
+      <div className="flex gap-4 text-xl">
+        <Link to="/">Home</Link>
+
+        <Link to="/cart">Cart</Link>
+
+        <Link to="/login">Login</Link>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
