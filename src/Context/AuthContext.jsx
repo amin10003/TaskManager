777b64fbe3571b6e28cfbaee62b5716f758f70ba
nimrod <nimrodtitus12@gmail.com>
2026-05-17 00:44:21 +0300

    Create authentication context

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,1 +1,17 @@
+import { createContext, useState } from "react";
 
+export const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
