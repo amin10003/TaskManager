10e60f2bb9a70265ce97073650ac910012043510
nimrod <nimrodtitus12@gmail.com>
2026-05-17 00:47:32 +0300

    Create Cart Context provider

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -2,32 +2,14 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext();
 
-export function createContext({ children }) {
+function CartProvider({ children }) {
   const [cartItems, setCartItems] = useState([]);
 
-  // Add product to cart
-  const addToCart = (product) => {
-    setCartItems([...cartItems, product]);
-  };
-
-  // Remove product from cart
-  const removeFromCart = (id) => {
-    const updatedCart = cartItems.filter(
-      (item) => item.id !== id
-    );
-
-    setCartItems(updatedCart);
-  };
-
   return (
-    <CartContext
-      value={{
-        cartItems,
-        addToCart,
-        removeFromCart,
-      }}
-    >
+    <CartContext.Provider value={{ cartItems, setCartItems }}>
       {children}
-    </CartContext>
+    </CartContext.Provider>
   );
 }
+
+export default CartProvider;
