355f9b080ec1b05c73436cd807ef2383bf66af4e
amin10003 <maxamedxasancali10003@gmail.com>
2026-05-18 06:54:01 +0300

    add navbar authentication state and logout functionality

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,20 +1,32 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
+
 import { CartContext } from "@/Context/CartContext";
+import { AuthContext } from "@/Context/AuthContext";
 
 function Navbar() {
   const { cartItems } = useContext(CartContext);
+
+  const { user, logout } = useContext(AuthContext);
 
   return (
     <nav className="bg-black text-white p-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">E-commerce</h1>
 
-      <div className="flex gap-4 text-xl">
+      <div className="flex gap-4 items-center">
         <Link to="/">Home</Link>
 
         <Link to="/cart">Cart ({cartItems.length})</Link>
 
-        <Link to="/login">Login</Link>
+        {user && <p className="text-sm">{user}</p>}
+
+        {!user && <Link to="/login">Login</Link>}
+
+        {user && (
+          <button onClick={logout} className="border px-3 py-1 rounded-lg">
+            Logout
+          </button>
+        )}
       </div>
     </nav>
   );
