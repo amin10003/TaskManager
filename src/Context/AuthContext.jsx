745686425e8bd7a664ab2123d0c580aed8587085
amin10003 <maxamedxasancali10003@gmail.com>
2026-05-18 06:59:03 +0300

    persist authentication state using localStorage

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,18 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(localStorage.getItem("user") || null);
 
   const login = (email) => {
     setUser(email);
+
+    localStorage.setItem("user", email);
   };
 
   const logout = () => {
     setUser(null);
-  };
 
+    localStorage.removeItem("user");
+  };
   return (
     <AuthContext.Provider
       value={{
